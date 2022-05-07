14f0369513290a49aff2332ac505a5ab5bafd966
Yevhenii Opryshko <yevhenii.opryshko@lanars.com>
2022-05-07 14:21:08 +0300

    Initial setup

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by Yevhenii on 07.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
